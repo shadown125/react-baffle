aeb59e8a126febc21a574a4bf78423ecb1c020a4
Dawid Oleksiuk <46052374+shadown125@users.noreply.github.com>
2024-08-17 14:09:04 +0200

    Fix lint errors

---
+++ .changeset/rich-apricots-tan.md
@@ -0,0 +1,5 @@
+---
+"react-baffle": patch
+---
+
+Fix lint erros
--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Baffle } from "./../../../../dist";
-
 export default function Home() {
-  return <Baffle speed={50}>ldkjflskdjfal</Baffle>;
+  return <></>;
 }
